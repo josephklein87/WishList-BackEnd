ba2eeeb1d6fdadd1503f97f897d1dc6b9ba55931
loganbeckham <loganbeckham1@gmail.com>
2023-01-05 16:09:27 -0600

    Cleared previous migrations for auth_api

--- auth_api/models.py
+++ auth_api/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class UserAccount(models.Model):
     email = models.CharField(max_length=75, unique=True)
     password = models.CharField(max_length=1000)
-    username = models.CharField(max_length=20, default='admin')
-    birthday = models.CharField(max_length=25, default='null')
+    username = models.CharField(max_length=12, unique=True)
+    birthday = models.CharField(max_length=25)
 
 class Follow(models.Model):
-    user = models.ForeignKey(UserAccount, related_name='following', on_delete=models.CASCADE, default='null')
+    user = models.ForeignKey(UserAccount, related_name='following', on_delete=models.CASCADE)
     following = models.ForeignKey(UserAccount, related_name='followers', on_delete=models.CASCADE)
