cd4b7fe618e1b4f90de162d6d3c0bcea21f88206
Joe Klein SEI <josephklein87@gmail.com>
2023-01-05 18:32:58 -0500

    fixed error with my merge

--- auth_api/urls.py
+++ auth_api/urls.py
@@ -5,7 +5,8 @@ from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
     path('api/useraccount', views.UserAccountList.as_view(), name='useraccount_list'),
     path('api/useraccount/<int:pk>', views.UserAccountDetail.as_view(), name='useraccount_detail'),
-    path('api/useraccount/login', csrf_exempt(views.check_login), name="check_login") # api/useraccount/login will be routed to the check_login function for auth
+    path('api/useraccount/login', csrf_exempt(views.check_login), name="check_login"), # api/useraccount/login will be routed to the check_login function for auth
     path('api/useraccount/search', views.user_search, name='user_search'),
     path('api/follow', views.FollowList.as_view(), name='follow_list'),
     path('api/follow/<int:pk>', views.FollowDetail.as_view(), name='follow_detail'),
+]
