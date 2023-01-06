9b32d53f9f79bf0789055b8d5726c8f501104c09
Joe Klein SEI <josephklein87@gmail.com>
2023-01-06 11:31:50 -0500

    changed price model to decimal

--- wishlist_api/models.py
+++ wishlist_api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Wishlist(models.Model):
    gift_picture = models.TextField(max_length=255)
    gift_name = models.CharField(max_length=50)
-   gift_price = models.IntegerField()
+   gift_price = models.DecimalField(decimal_places=2, max_digits=9)
    on_sale = models.BooleanField(default=False)
    link = models.TextField(max_length=1000)
    tags = models.CharField(max_length=50)
